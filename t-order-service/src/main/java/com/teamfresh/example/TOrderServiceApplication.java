16c866dd381d387f18ca3946c9d8e9eb1e464d75
honinbo6355 <honinbo6355@nate.com>
2024-09-08 17:10:02 +0900

    initial commit

---
+++ t-order-service/src/main/java/com/teamfresh/example/TOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.teamfresh.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TOrderServiceApplication.class, args);
+	}
+
+}
---
+++ t-order-service/src/test/java/com/teamfresh/example/TOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamfresh.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
